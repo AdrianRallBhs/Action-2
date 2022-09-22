1623b628288eea5df6e749aefcadf611b96bddb6
Adrian Rall <adrian@asda4.de>
2022-09-22 15:02:37 +0200

    Tests hinzugefügt

---
+++ Action2BlazorApp/Action2BlazorApp.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Action2BlazorApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestF2C()
+        {
+            double f = 32;
+            double c = 0;
+            double rvalue = Convert.Convert.f2c(f);
+            Assert.AreEqual(c, rvalue);
+            Assert.AreEqual(100, Convert.Convert.f2c(212));
+        }
+
+        [TestMethod]
+        public void TestC2F()
+        {
+            Assert.AreEqual(212, Convert.Convert.c2f(100));
+            Assert.AreEqual(32, Convert.Convert.c2f(0));
+        }
+    }
+}
---
+++ Action2BlazorApp/Action2BlazorApp/Convert.cs
@@ -0,0 +1,15 @@
+﻿namespace Convert
+{
+    public class Convert
+    {
+        public static double f2c(double f)
+        {
+            return (f - 32) * 5 / 9;
+        }
+
+        public static double c2f(double c)
+        {
+            return (c * 9 / 5) + 32;
+        }
+    }
+}
