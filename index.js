6b269952381e9dc0f88bcb6d9905b37124c2791f
Adrian Rall <adrian@asda4.de>
2022-09-23 14:00:55 +0200

    index.js und jode.js installiert?

---
+++ index.js
@@ -0,0 +1,9 @@
+const core = require('@actions/core');
+
+try{
+  const firstname = core.getInput("firstname");
+  console.log('Hello ${firstname}!');
+}
+catch(error) {
+  core.setFailed(error.message);
+}
